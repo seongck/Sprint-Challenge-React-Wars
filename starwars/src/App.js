a7cc47012323d4279a37374143472026d564759d
seongck <seongk@pm.me>
2019-08-02 18:46:22 -0700

    Add 'people' state properties

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [ people, setPeople ] = useState(null); 
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
