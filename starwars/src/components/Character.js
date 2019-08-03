da80c401fc80a71888f7d411db722b5c4838d16f
seongck <seongk@pm.me>
2019-08-02 20:04:17 -0700

    Adjust Card styles

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,28 +7,30 @@ const capitalizeFirstLetter = (string) =>
 const Card = styled.div`
   background: rgba(255 ,255, 255, 0.7);  
   color: rgba(0, 0, 0, 0.7);
-  padding: 1% 2%;
+  padding: 1% 3% 2%;
   margin: 10px;
   border-radius: 5px;
   text-align: left;
-  width: 14%;
+  width: 17%;
 `;
 
 const Name = styled.h2`
   font-family: 'Saira Stencil One', cursive;
+  text-align: center;
+  margin-bottom: 5px;
 `;
 
 export default function Character({info}) {
   return (
     <Card>
       <Name>{info.name}</Name>
-      <p>Height: {info.height} </p>
-      <p>Mass: {info.mass} </p>
-      <p>Hair Color: {capitalizeFirstLetter(info.hair_color)}</p>
-      <p>Skin Color: {capitalizeFirstLetter(info.skin_color)}</p>
-      <p>Eye Color: {capitalizeFirstLetter(info.eye_color)}</p>
-      <p>Birth Year: {info.birth_year}</p>
-      <p>Gender: {capitalizeFirstLetter(info.gender)}</p>
+      <p><strong>Height:</strong> {info.height} </p>
+      <p><strong>Mass:</strong> {info.mass} </p>
+      <p><strong>Hair Color:</strong> {capitalizeFirstLetter(info.hair_color)}</p>
+      <p><strong>Skin Color:</strong> {capitalizeFirstLetter(info.skin_color)}</p>
+      <p><strong>Eye Color:</strong> {capitalizeFirstLetter(info.eye_color)}</p>
+      <p><strong>Birth Year:</strong> {info.birth_year}</p>
+      <p><strong>Gender:</strong> {capitalizeFirstLetter(info.gender)}</p>
     </Card>
   )
 }
